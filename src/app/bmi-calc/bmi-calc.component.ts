e95e13f8c0aa5836802a01e63a6e1fb4d1c38eb3
Emirhan AKSU <emirhanaksu1903@gmail.com>
2023-07-01 00:54:15 +0300

    Uyarılar eklendi

--- src/app/bmi-calc/bmi-calc.component.ts
+++ src/app/bmi-calc/bmi-calc.component.ts
@@ -9,16 +9,26 @@ export class BmiCalcComponent implements OnInit{
   weight!:number;
   height!:number;
   bmi!:number;
+  nonItems = false;
 
 
   calculateBmi(){
+      if (this.weight == null || this.height == null) {
+        this.nonItems = true
+        setTimeout(() => {
+          this.nonItems = false;
+        }, 3000);
+        return;
+      }
+      else{
+        const heightInMeters = this.height/100;
+        this.bmi = this.weight /(heightInMeters*heightInMeters)
+        console.log("Bmi "+ this.bmi.toFixed(2))
+        return this.bmi.toFixed(2);
 
-    const heightInMeters = this.height/100;
-    this.bmi = this.weight /(heightInMeters*heightInMeters)
-    console.log("Bmi "+ this.bmi.toFixed(2))
-    return this.bmi.toFixed(2);
-
+      }
   }
+  
 
   ngOnInit(): void {}
 }
